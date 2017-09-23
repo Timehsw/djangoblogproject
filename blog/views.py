c9c3f87e691a6b08d21cedf6bb89759e98afba38
HswTime <hsw.time@gmail.com>
2017-09-23 21:21:11 +0800

    add detail and aboutme

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post,Category,Tag
-
+import markdown
 
 def index(request):
     '''
@@ -12,3 +12,15 @@ def index(request):
     post_list=Post.objects.all().order_by('-created_time')
     return render(request,'blog/index.html',context={'post_list':post_list})
 
+def detail(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    post.body=markdown.markdown(post.body,
+                                extensions=[
+                                    'markdown.extensions.extra',
+                                    'markdown.extensions.codehilite',
+                                    'markdown.extensions.toc',
+                                ])
+    return render(request,'blog/detail.html',context={'post':post})
+
+def about(request):
+    return render(request,'blog/about.html')
