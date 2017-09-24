472857bd9c56f7075506c601b9486ae0c52eca11
HswTime <hsw.time@gmail.com>
2017-09-24 08:55:25 +0800

    add category and archives

--- blog/views.py
+++ blog/views.py
@@ -43,6 +43,12 @@ def archives(request,year,month):
     return render(request,'blog/index.html',context={'post_list':post_list})
 
 def category(request,pk):
+    '''
+    博客分类
+    :param request:
+    :param pk:
+    :return:
+    '''
     cate=get_object_or_404(Category,pk=pk)
     post_list=Post.objects.filter(category=cate).order_by('-created_time')
     return render(request,'blog/index.html',context={'post_list':post_list})
